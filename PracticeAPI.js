fb69a83b22d69b2c1a9ab15e0b2a3c7097afabb3
CryptoN999 <nicholasgallucci4@gmail.com>
2020-01-20 13:07:13 -0800

    express node server

--- PracticeAPI.js
+++ PracticeAPI.js
@@ -1,14 +1,28 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", 'text/plain');
+//     res.end('Hello World, from NodeJS');
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`server listening on http://${hostname}:${port}`);
+// })
+
+const express = require('express');
+
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", 'text/plain');
-    res.end('Hello World, from NodeJS');
-});
+const server = express();
 
-server.listen(port, hostname, () => {
-    console.log(`server listening on http://${hostname}:${port}`);
+server.get('/', (req, res) => {
+    res.send('Hello world from Express!')
+})
+
+server.listen(port, () => {
+    console.log(`server listening on ${port}`);
 })
